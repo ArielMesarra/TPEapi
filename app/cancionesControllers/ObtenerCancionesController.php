8b990735b6ae36bf54c9077eae8acd65e611c85b
ArielMesarra <arielmesarra28@gmail.com>
2022-11-14 17:12:21 -0300

    Terminado los order, limit, sort

--- app/cancionesControllers/ObtenerCancionesController.php
+++ app/cancionesControllers/ObtenerCancionesController.php
@@ -5,7 +5,11 @@ require_once "app/views/JsonApiView.php";
 class ObtenerCancionesController {
     private $model;
     private $view;
-
+    protected $desde = 0;
+    protected $hasta = 500;
+    protected $columna = "nombre";
+    protected $order = "ASC";
+    
     function __construct() {
         $this->model = new CancionesModel();
         $this->view = new JSONAPIView();
@@ -13,15 +17,11 @@ class ObtenerCancionesController {
 
     function obtenerCanciones($params = []) {
         if (empty($params)) {
-            $d = 0;
-            $h = 500;
-            if(isset($_GET["desde"])&&isset($_GET["hasta"])){
-                $d = $_GET["desde"];
-                $h = $_GET["hasta"];
-            }
-                $canciones = $this->model->obtenerCanciones($d, $h);
-                $this->view->response($canciones, 200);
-            }
+            $referencia = $this->model->obtenerCanciones(0,1,"nombre", "ASC");
+            $this->verificar($referencia, 'id_canciones');
+            $canciones = $this->model->obtenerCanciones($this->desde, $this->hasta, $this->columna, $this->order);
+            $this->view->response($canciones, 200);
+        }
         else {
             $cancion = $this->model->obtenerCancion($params[":ID"]);
             if(!empty($cancion)) {
@@ -31,5 +31,24 @@ class ObtenerCancionesController {
                 $this->view->response("No se encontro la cancion con el id: ".$params[":ID"], 404);
             }
         }
+        
+    }
+    function verificar($referencia, $columna){
+        $por = "";
+        if(isset($_GET["desde"])&&isset($_GET["hasta"])){
+            $this->desde = $_GET["desde"];
+            $this->hasta = $_GET["hasta"];
+        }
+
+        if(isset($_GET["por"])){
+            $por = $this->columna = $_GET["por"];
+        }
+        
+        //TRAEMOS UN REGISTRO PARA VER SI LO QUE EL USUARIO PUSO EN LA URL PARA ORDENAR POR COLUMNA, Y SI EL CAMPO COINCIDE CON EL DE LA DB.
+        if(empty($referencia[0]->$por)){
+            $this->columna = $columna;
+        }
+        
+        if(isset($_GET['order'])&&$_GET['order']=="DESC")$this->order = "DESC";
     }
 }
--- app/models/CancionesModel.php
+++ app/models/CancionesModel.php
@@ -6,12 +6,10 @@ class CancionesModel{
             $this->db = new PDO('mysql:host=localhost;'.'dbname=biblioteca_bd;charset=utf8', 'root', '');
         }
         
-        function obtenerCanciones($d, $h){
-            $columna = "nombre";
-            $query=$this->db->prepare('SELECT c.*,a.nombre AS nombreDeArtista FROM canciones AS c INNER JOIN artistas AS a ON c.fk_id_artistas = a.id_artistas ORDER BY :columna LIMIT :desde, :hasta ');
+        function obtenerCanciones($d, $h, $columna, $order){
+            $query = $this->db->prepare('SELECT c.*,a.nombre AS nombreDeArtista FROM canciones AS c INNER JOIN artistas AS a ON c.fk_id_artistas = a.id_artistas ORDER BY '.$columna.' '.$order.' LIMIT :desde, :hasta');
             $query->bindParam(':desde', $d, PDO::PARAM_INT);
             $query->bindParam(':hasta', $h, PDO::PARAM_INT);
-            $query->bindParam(':columna', $columna, PDO::PARAM_STR);
             $query->execute();
             $canciones = $query->fetchAll(PDO::FETCH_OBJ);
             return $canciones;
